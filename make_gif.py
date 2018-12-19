9ee615fc21a8f23985f6449d19954f20fee3e05e
Richard Podkolinski <devlar@gmail.com>
2018-12-19 10:09:25 +0100

    Added file type arguments

--- make_gif.py
+++ make_gif.py
@@ -2,7 +2,9 @@ import argparse
 import imageio
 import os
 
+
 def parse_args():
+
     desc = "Makes an animated gif from a folder of pictures"  
     parser = argparse.ArgumentParser(description=desc)
 
@@ -21,33 +23,35 @@ def parse_args():
     parser.add_argument('--duration', type=float,
         default=0.13,
         help='Duration between frames in the animated gif (default: %(default)s)')
+
+    parser.add_argument('--types', nargs='+', type=str,
+        default=['png', 'jpg'],
+        help='The file types to consider when making the animated gifs (default: %(default)s)')
     
     args = parser.parse_args()
     return args
 
 
 def make_gif(img_folder, output_file, duration, verbose):
-
-    output_folder = os.path.join(img_folder)
-    if args.verbose: print(output_folder)
     
-    files = os.listdir(output_folder)
-    if args.verbose: print(files)
-
-    filenames = [os.path.join(output_folder, f) for f in files if f.endswith(('.png'))]
-
+    if args.verbose: print("Image Folder: ", img_folder, '\n')
+    
+    files = os.listdir(img_folder)
+    if args.verbose: print("Input Files: \n", files, '\n')
+    
+    filenames = [os.path.join(img_folder, f) for f in files if f.endswith(tuple(args.types))]
+    if args.verbose: print("The following file types are used: ", tuple(args.types), '\n')
+    
     images = list(map(lambda filename: imageio.imread(filename), filenames))
     
     imageio.mimsave(output_file, images, duration = 0.13)
-    if args.verbose: print('Wrote %s' % output_file )
-
-
+    if args.verbose: print('Wrote %s' % output_file)
 
 
 def main():
     global args
     args = parse_args()
-    if args.verbose: print(args)
+    if args.verbose: print(args, '\n')
     make_gif(args.img_folder, args.output_file, args.duration, args.verbose)
     
 
