bca4088a4d7b78cef939a49ebe900f2a1763a503
Richard Podkolinski <devlar@gmail.com>
2018-12-19 10:20:34 +0100

    Added option to shuffle images rather than do it alphabetically

--- make_gif.py
+++ make_gif.py
@@ -1,6 +1,7 @@
 import argparse
 import imageio
 import os
+import random
 
 
 def parse_args():
@@ -27,32 +28,41 @@ def parse_args():
     parser.add_argument('--types', nargs='+', type=str,
         default=['png', 'jpg'],
         help='The file types to consider when making the animated gifs (default: %(default)s)')
+
+    parser.add_argument('--shuffle', 
+        action='store_true',
+        help='Boolean flag indicating if statements should be printed to the console.')
     
     args = parser.parse_args()
     return args
 
 
-def make_gif(img_folder, output_file, duration, verbose):
+def make_gif(img_folder, output_file, duration, types, verbose, shuffle):
     
-    if args.verbose: print("Image Folder: ", img_folder, '\n')
+    if verbose: print("Image Folder: ", img_folder, '\n')
     
     files = os.listdir(img_folder)
-    if args.verbose: print("Input Files: \n", files, '\n')
+    if verbose: print("Input Files: \n", files, '\n')
     
-    filenames = [os.path.join(img_folder, f) for f in files if f.endswith(tuple(args.types))]
-    if args.verbose: print("The following file types are used: ", tuple(args.types), '\n')
+    filenames = [os.path.join(img_folder, f) for f in files if f.endswith(tuple(types))]
+    if verbose: print("The following file types are used: ", tuple(types), '\n')
+
+    if shuffle: random.shuffle(filenames)
+    if verbose: print("Shuffling images\n")
     
     images = list(map(lambda filename: imageio.imread(filename), filenames))
     
+    if os.path.exists(output_file): os.remove(output_file)
+    
     imageio.mimsave(output_file, images, duration = 0.13)
-    if args.verbose: print('Wrote %s' % output_file)
+    if verbose: print('Wrote %s' % output_file)
 
 
 def main():
     global args
     args = parse_args()
     if args.verbose: print(args, '\n')
-    make_gif(args.img_folder, args.output_file, args.duration, args.verbose)
+    make_gif(args.img_folder, args.output_file, args.duration, args.types, args.verbose, args.shuffle)
     
 
 if __name__ == '__main__':
