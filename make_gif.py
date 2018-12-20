b62f78ae2550a8adffb7e7ea8eccb1b9d84482b1
Richard Podkolinski <devlar@gmail.com>
2018-12-20 18:50:45 +0100

    Added short arguments

--- make_gif.py
+++ make_gif.py
@@ -12,25 +12,29 @@ def parse_args():
     parser.add_argument('img_folder', type=str,
         help='The folder name that contains images to be made into a gif.')
 
-    parser.add_argument('--verbose', 
+    parser.add_argument('-v', '--verbose', 
         action='store_true',
         help='Boolean flag indicating if statements should be printed to the console.')
 
-    parser.add_argument('--output_file', type=str,
+    parser.add_argument('-o', '--output_file', type=str,
         default='animation.gif',
         help='The output file of the animated gif (default: %(default)s).')
-    
-    parser.add_argument('--duration', type=float,
-        default=0.13,
-        help='Duration between frames in the animated gif (default: %(default)s).')
 
-    parser.add_argument('--types', nargs='+', type=str,
+    parser.add_argument('-t','--types', nargs='+', type=str,
         default=['png', 'jpg'],
         help='The file types to consider when making the animated gifs (default: %(default)s).')
 
-    parser.add_argument('--shuffle', 
+    parser.add_argument('-s', '--shuffle', 
         action='store_true',
         help='Shuffles the order of the files that make up the animated gif.')
+    
+    group = parser.add_mutually_exclusive_group()
+
+    group.add_argument('-d', '--duration', type=float,
+        default=0.13,
+        help='Duration between frames in the animated gif (default: %(default)s).')
+    
+    # @TODO Speed Argument using count and add to group
     
     args = parser.parse_args()
     return args
